de875d90788b28b45657026a0d5484cdbf4e50b6
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-10-04 08:55:33 -0300

    plop config

---
+++ plop/plopfile.js
@@ -0,0 +1,45 @@
+module.exports = (plop) => {
+  plop.setGenerator('component', {
+    description: "Criar componente",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Digite o nome do seu componente"
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/index.tsx',
+        templateFile: "templates/Component.tsx.hbs"
+      },
+      {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/_style.scss'
+      },
+    ]
+  });
+
+  plop.setGenerator('page', {
+    description: "Criar página",
+    prompts: [
+      {
+        type: "input",
+        name: "name",
+        message: "Digite o nome da página"
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: '../src/app/{{lowerCase name}}/index.tsx',
+        templateFile: "templates/Page.tsx.hbs"
+      },
+      {
+        type: 'add',
+        path: '../src/app/{{lowerCase name}}/_style.scss'
+      },
+    ]
+  })
+};
