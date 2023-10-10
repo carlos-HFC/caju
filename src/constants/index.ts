d0a2da1e60c69e0e4b547757d7d9406c704ca124
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-10-10 11:01:26 -0300

    constant for carousel items

---
+++ src/constants/index.ts
@@ -0,0 +1,26 @@
+export const CAROUSEL_ITEMS = [
+  {
+    img: '/egg.png',
+    title: "4 substitutos de ovos",
+    description: "Você sabe quais são as diferenças entre os estilos de vida?",
+    category: "Conceituação"
+  },
+  {
+    img: '/cow.png',
+    title: "Vegano e vegetariano",
+    description: "Você sabe quais são as diferenças entre os estilos de vida?",
+    category: "Conceituação"
+  },
+  {
+    img: '/matheus.png',
+    title: "Meu relato no veganismo",
+    description: "Você sabe quais são as diferenças entre os estilos de vida?",
+    category: "Relato"
+  },
+  {
+    img: '/milk.png',
+    title: "Leites vegetais",
+    description: "Você sabe quais são as diferenças entre os estilos de vida?",
+    category: "Substituições"
+  },
+]
