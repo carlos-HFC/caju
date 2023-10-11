f9121fe2ffb41d4e6890dc222199362f38a8e4cd
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-10-11 09:44:11 -0300

    prop link in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,3 +1,5 @@
+import Link, { LinkProps } from 'next/link';
+
 import { Icon } from "../Icon";
 
 type ButtonVariant = 'primary' | 'secondary' | 'outline-primary' | 'outline-secondary' | 'attach' | 'loading' | 'attached';
@@ -5,14 +7,21 @@ type ButtonVariant = 'primary' | 'secondary' | 'outline-primary' | 'outline-seco
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children?: React.ReactNode;
   variant?: ButtonVariant;
+  href?: LinkProps['href'];
 }
 
 export function Button(props: Readonly<ButtonProps>) {
   return (
-    <button className={["c-button", `c-button-${props.variant ?? 'primary'}`, props.disabled && 'disabled'].join(' ')} aria-disabled={props.disabled} {...props}>
-      {props.variant !== 'loading'
-        ? props.children
-        : <Icon name="loading" />}
-    </button>
+    props.href ? (
+      <Link href={props.href} className={["c-button", `c-button-${props.variant ?? 'primary'}`, props.disabled && 'disabled'].join(' ')} aria-disabled={props.disabled}>
+        {props.children}
+      </Link>
+    ) : (
+      <button className={["c-button", `c-button-${props.variant ?? 'primary'}`, props.disabled && 'disabled'].join(' ')} aria-disabled={props.disabled} {...props}>
+        {props.variant !== 'loading'
+          ? props.children
+          : <Icon name="loading" />}
+      </button>
+    )
   );
 }
