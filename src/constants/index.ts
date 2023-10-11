d9ff8a17f5449a1b038a12413928035bb97a5404
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-10-11 11:06:34 -0300

    add project component and constant for project items

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -39,3 +39,21 @@ export const CARD_PRICE_LIST = [
   'Acesso ilimitado',
   'Traduzido em libras',
 ];
+
+export const PROJECTS_ITEMS = [
+  {
+    title: "Bicharada Universitária",
+    body: "Alunos, professores e técnicos da Universidade Federal de Rio Grande se uniram para cuidar, castrar, alimentar e encontrar um lar amoroso para animais abandonados no campus.",
+    image: "/bicharada.png"
+  },
+  {
+    title: "Rancho dos Gnomos",
+    body: "O santuário acolhe animais vítimas de maus-tratos, matadouros clandestinos, rinhas, circos, rodeios e tráfico de animais para que vivam com dignidade e livres de exploração.",
+    image: "/rancho-gnomos.png"
+  },
+  {
+    title: "Movimento Afro Vegano",
+    body: "Plataforma que discute racismo, especismo e veganismo dentro da comunidade afro, proporcionando informação e o  resgate do protagonismo preto na alimentação.",
+    image: "/movimento-afro.png"
+  },
+];
