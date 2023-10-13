a812e7af5e171c9d735795257504af36ef5ecdd2
Carlos Henrique Faustino Cardoso <carlos.cardoso@compasso.com.br>
2023-10-13 09:26:03 -0300

    add background image banner

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,17 @@
+import Image from "next/image";
+
 import { Banner } from "@/components/Banner";
 
 export default function Home() {
   return (
     <>
+      <Image
+        src="/banner.png"
+        width={1240}
+        height={1551}
+        alt="Imagem de banner"
+        className="image-banner"
+      />
       <Banner />
     </>
   );
