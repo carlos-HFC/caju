69d6d79a135d658b0aadf642d835a2611a666d5f
Carlos Faustino <chfcchfc96@gmail.com>
2023-11-21 11:31:41 -0300

    remove character

--- src/utils/mask.ts
+++ src/utils/mask.ts
@@ -6,41 +6,41 @@ export function mask(type: MaskType, value: string) {
       return value
         ?.replace(/\D/g, '')
         ?.replace(/(\d{5})(\d)/, '$1-$2')
-        ?.replace(/(-\d{3})\d+?$/, '$1');
+        ?.replace(/(-\d{3})\d+$/, '$1');
     case "cpf":
       return value
         ?.replace(/\D/g, "")
         ?.replace(/(\d{3})(\d)/, "$1.$2")
         ?.replace(/(\d{3})(\d)/, "$1.$2")
         ?.replace(/(\d{3})(\d{2})/, "$1-$2")
-        ?.replace(/(-\d{2})\d+?$/, "$1");
+        ?.replace(/(-\d{2})\d+$/, "$1");
     case "phone":
       if (value?.length === 14)
         return value
           ?.replace(/\D/g, "")
           ?.replace(/(\d{2})(\d)/, "($1) $2")
           ?.replace(/(\d{4})(\d)/, "$1-$2")
-          ?.replace(/(-\d{4})\d+?$/, "$1");
+          ?.replace(/(-\d{4})\d+$/, "$1");
       return value
         ?.replace(/\D/g, "")
         ?.replace(/(\d{2})(\d)/, "($1) $2")
         ?.replace(/(\d{5})(\d)/, "$1-$2")
-        ?.replace(/(-\d{4})\d+?$/, "$1");
+        ?.replace(/(-\d{4})\d+$/, "$1");
     case "date":
       return value
         ?.replace(/\D/g, "")
         ?.replace(/(\d{2})(\d)/, "$1/$2")
-        ?.replace(/(\/\d{4})\d+?$/, "$1");
+        ?.replace(/(\/\d{4})\d+$/, "$1");
     case "card":
       return value
         ?.replace(/\D/g, "")
         ?.replace(/(\d{4})(\d)/, "$1 $2")
         ?.replace(/(\d{4})(\d)/, "$1 $2")
         ?.replace(/(\d{4})(\d)/, "$1 $2")
-        ?.replace(/(\s\d{4})\d+?$/, "$1");
+        ?.replace(/(\s\d{4})\d+$/, "$1");
     case "cvv":
       return value
         ?.replace(/\D/g, '')
-        ?.replace(/(\d{3})\d+?$/, '$1');
+        ?.replace(/(\d{3})\d+$/, '$1');
   }
 }
